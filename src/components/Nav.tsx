27f3a610563c96c828841661af8ab0fb49d76096
Tyler Newman <tylerjnewman18@gmail.com>
2022-08-24 15:16:45 -0700

    Make logo responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,13 @@
 import {Box, Flex, Stack} from '@chakra-ui/react'
 import {DarkModeSwitch} from './DarkModeSwitch'
+import {Logo} from './Logo'
 
 export default function Nav() {
   return (
     <>
       <Box px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>Logo</Box>
+          <Logo />
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
