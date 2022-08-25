6220e8268b716ea4c6bc45c4353bb61b2cd6f0d5
Tyler Newman <tylerjnewman18@gmail.com>
2022-08-24 22:28:26 -0700

    More Cleanup

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,24 +1,13 @@
 import {
   Box,
-  Button,
-  ButtonGroup,
-  Container,
-  HStack,
-  Icon,
-  Input,
-  InputGroup,
-  InputLeftElement,
   Stack,
-  Text,
   useBreakpointValue,
   useColorModeValue,
 } from '@chakra-ui/react'
 import * as React from 'react'
-import {FiSearch} from 'react-icons/fi'
 import {MemberTable} from './MemberTable'
 
 export const Table = () => {
-  const isMobile = useBreakpointValue({base: true, md: false})
   return (
     <Box
       bg="bg-surface"
