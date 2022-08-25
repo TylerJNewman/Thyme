7621001cbd60e859097f25febdd875f0804b3c59
Tyler Newman <tylerjnewman18@gmail.com>
2022-08-25 15:57:36 -0700

    Adjust Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,20 @@
-import {Box, Flex, Stack} from '@chakra-ui/react'
+import {Box, Container, Flex, Stack} from '@chakra-ui/react'
 import {DarkModeSwitch} from './DarkModeSwitch'
 import {Logo} from './Logo'
 
 export default function Nav() {
   return (
-    <>
-      <Box px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+    <Box as="nav">
+      <Container py={{base: '3', lg: '4'}}>
+        <Flex justify="space-between">
           <Logo />
-
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
               <DarkModeSwitch />
             </Stack>
           </Flex>
         </Flex>
-      </Box>
-    </>
+      </Container>
+    </Box>
   )
 }
