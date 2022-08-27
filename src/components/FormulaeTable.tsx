11e581f0c25d920427b2502394372b52a99b130a
Tyler Newman <tylerjnewman18@gmail.com>
2022-08-27 14:04:44 -0700

    Add modal for formula info

--- src/components/FormulaeTable.tsx
+++ src/components/FormulaeTable.tsx
@@ -9,63 +9,112 @@ import {
   Text,
   Tr,
   Skeleton,
+  ModalOverlay,
+  useDisclosure,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
 } from '@chakra-ui/react'
-import {useContext} from 'react'
+import {useContext, useState, forwardRef} from 'react'
 import {TableVirtuoso} from 'react-virtuoso'
 
 import {LogoSmall} from './LogoSmall'
 import {FormulaeContext} from '../context/FormulaaContext'
 import ChakraNextLink from './ChakraNextLink'
-import React from 'react'
 import {useScrollTable} from '../context/ScrollTableContext'
 
-const Row = ({index, data}: {index: number; data: any}) => (
-  <>
-    <Td width="100%">
-      <Fade in={true}>
-        <HStack spacing="3">
-          <IconButton
-            variant="ghost"
-            icon={<LogoSmall fontSize="1.25rem" />}
-            aria-label="Homepage"
-          />
-          <Box>
-            <Text fontWeight="medium" fontSize={{sm: 'inherit', md: 'md'}}>
-              {data[index].full_name}
-            </Text>
-            <Text color="muted" fontSize={{sm: 'inherit', md: 'md'}}>
-              {data[index].versions?.stable}
-            </Text>
-          </Box>
-        </HStack>
-      </Fade>
-    </Td>
-    <Td
-      style={{
-        textAlign: 'end',
-      }}
-    >
-      <Fade in={true}>
-        <ChakraNextLink href={data[index].homepage ?? ''} isExternal>
-          <Box>
-            <Text color="muted" fontSize={{sm: 'inherit', md: 'md'}}>
-              {data[index].desc}
-            </Text>
-          </Box>
-        </ChakraNextLink>
-      </Fade>
-    </Td>
-  </>
+const OverlayOne = () => (
+  <ModalOverlay
+    bg="blackAlpha.300"
+    backdropFilter="blur(10px) hue-rotate(90deg)"
+  />
+)
+
+const Row = ({index, data, setOverlay, onOpen}: any) => {
+  return (
+    <>
+      <Td width="100%">
+        <Fade in={true}>
+          <HStack spacing="3">
+            <IconButton
+              variant="ghost"
+              icon={<LogoSmall fontSize="1.25rem" />}
+              aria-label="formula-info"
+              onClick={() => {
+                setOverlay(<OverlayOne />)
+                onOpen()
+              }}
+            />
+            <Box>
+              <Text fontWeight="medium" fontSize={{sm: 'inherit', md: 'md'}}>
+                {data[index].full_name}
+              </Text>
+              <Text color="muted" fontSize={{sm: 'inherit', md: 'md'}}>
+                {data[index].versions?.stable}
+              </Text>
+            </Box>
+          </HStack>
+        </Fade>
+      </Td>
+      <Td
+        style={{
+          textAlign: 'end',
+        }}
+      >
+        <Fade in={true}>
+          <ChakraNextLink href={data[index].homepage ?? ''} isExternal>
+            <Box>
+              <Text color="muted" fontSize={{sm: 'inherit', md: 'md'}}>
+                {data[index].desc}
+              </Text>
+            </Box>
+          </ChakraNextLink>
+        </Fade>
+      </Td>
+    </>
+  )
+}
+
+interface FormulaInfoModalProps {
+  isOpen: boolean
+  onClose: () => void
+  overlay: any
+}
+
+const FormulaInfoModal = ({
+  isOpen,
+  onClose,
+  overlay,
+}: FormulaInfoModalProps) => (
+  <Modal isCentered isOpen={isOpen} onClose={onClose}>
+    {overlay}
+    <ModalContent>
+      <ModalHeader>Modal Title</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>
+        <Text>Custom backdrop filters!</Text>
+      </ModalBody>
+      <ModalFooter>
+        <Button onClick={onClose}>Close</Button>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
 )
 
 export const FormulaeTable = () => {
+  const {isOpen, onOpen, onClose} = useDisclosure()
+  const [overlay, setOverlay] = useState(<OverlayOne />)
   const {data, error} = useContext(FormulaeContext)
 
   if (error) return <div>failed to load</div>
 
   const {ref} = useScrollTable()
 
-  console.log(data)
+  console.log(data.slice(0, 10))
 
   return (
     <>
@@ -82,15 +131,23 @@ export const FormulaeTable = () => {
           components={{
             Table: props => <Table {...props} />,
             TableRow: props => <Tr {...props} />,
-            TableBody: React.forwardRef((props, ref) => (
+            TableBody: forwardRef((props, ref) => (
               <Tbody {...props} ref={ref} />
             )),
           }}
           data={data}
           overscan={100}
-          itemContent={index => <Row index={index} data={data} />}
+          itemContent={index => (
+            <Row
+              index={index}
+              data={data}
+              setOverlay={setOverlay}
+              onOpen={onOpen}
+            />
+          )}
         />
       )}
+      <FormulaInfoModal isOpen={isOpen} onClose={onClose} overlay={overlay} />
     </>
   )
 }
