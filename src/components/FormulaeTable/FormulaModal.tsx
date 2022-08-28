50537276bdea2083e5963e106307c5a388829e5c
Tyler Newman <tylerjnewman18@gmail.com>
2022-08-27 20:28:30 -0700

    Clean up files

--- src/components/FormulaeTable/FormulaModal.tsx
+++ src/components/FormulaeTable/FormulaModal.tsx
@@ -6,21 +6,16 @@ import {
   ModalFooter,
   Button,
   Text,
-  Avatar,
-  Box,
   HStack,
-  SimpleGrid,
   Stack,
-  useBreakpointValue,
-  Badge,
   useColorModeValue,
-  Skeleton,
 } from '@chakra-ui/react'
 import {Octokit} from '@octokit/rest'
-import ChakraNextLink from 'components/ChakraNextLink'
+
 import {useEffect, useState} from 'react'
 import {getTwitterDate} from 'utils/getTwitterDate'
 import Header from './Header'
+import Contributors from './Contributors'
 
 const octokit = new Octokit({auth: process.env.NEXT_PUBLIC_GITHUB_TOKEN})
 
@@ -29,67 +24,6 @@ interface FormulaModalProps {
   onClose: () => void
   overlay: any
   formula: any
-}
-
-const Members = ({owner, repo}) => {
-  const [contributors, setContributors] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const getContributors = async () => {
-      const {data} = await octokit.repos.listContributors({
-        owner,
-        repo,
-      })
-      setContributors(data)
-      setLoading(false)
-    }
-    getContributors()
-  }, [owner, repo])
-
-  if (loading) return <Skeleton height="400px" />
-
-  if (!contributors.length) return null
-
-  return (
-    <SimpleGrid
-      columns={{base: 1, md: 2}}
-      columnGap="8"
-      rowGap={{base: '10', lg: '12'}}
-      flex="1"
-    >
-      {contributors?.map(contributor => (
-        <Stack
-          key={contributor.login}
-          spacing={{base: '4', md: '5'}}
-          direction="row"
-        >
-          <Avatar
-            src={contributor.avatar_url}
-            name={contributor.login}
-            boxSize={{base: '12', md: '16'}}
-          />
-          <Stack spacing="4">
-            <Stack>
-              <Box>
-                <ChakraNextLink href={contributor.html_url ?? ''} isExternal>
-                  <Text fontWeight="medium" fontSize="lg">
-                    {contributor.login}
-                  </Text>
-                </ChakraNextLink>
-                {contributor?.site_admin ? (
-                  <Badge colorScheme={'blue'}>Admin</Badge>
-                ) : null}
-              </Box>
-              <Text color="muted" fontSize={{base: 'sm'}}>
-                #commits: {contributor?.contributions.toLocaleString('en-US')}
-              </Text>
-            </Stack>
-          </Stack>
-        </Stack>
-      ))}
-    </SimpleGrid>
-  )
 }
 
 const FormulaModal = ({
@@ -196,7 +130,7 @@ const FormulaModal = ({
             >
               Collaborators
             </Text>
-            <Members owner={owner} repo={repo} />
+            <Contributors owner={owner} repo={repo} />
           </Stack>
         </ModalBody>
         <ModalFooter>
